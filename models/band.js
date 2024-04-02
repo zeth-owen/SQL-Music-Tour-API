85c9bf405a577a5759e6dd1cc294f1c76b4b0c82
Zeth Thomas <zeththomas89@gmail.com>
2024-04-01 21:19:48 -0500

    install cli

---
+++ models/band.js
@@ -0,0 +1,39 @@
+// DEPENDENCIES
+const { Sequelize, DataTypes,  Model } = require('sequelize')
+const sequelize = new Sequelize(process.env.PG_URI)
+
+Band.init({
+    band_id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true
+    },
+    name: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    genre: { 
+        type: DataTypes.TEXT, 
+        allowNull: false 
+    },
+    available_start_time: { 
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+    end_time: { 
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+}, {
+    sequelize,
+    modelName: 'Band',
+    tableNmae: 'band',
+    timeStamp: false
+}) 
+
+
+// MODEL
+class Band extends Model{}
+
+// EXPORT
+module.exports = Band
--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const { Sequelize } = require('sequelize');
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI);
+
+sequelize.authenticate()
+  .then(() => {
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`);
+  })
+  .catch(err => {
+    console.error(`Unable to connect to PG: ${err}`);
+  });
 
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
-    })
-})
+    });
+});
 
 // LISTEN
-app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🎸 Rockin' on port: ${PORT}`);
+});
+
 
 
 
